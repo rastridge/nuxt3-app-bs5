246e40dc9d32ba937215d940a6e59078687cb834
rastridge <rfa@me.com>
2022-11-23 18:51:26 -0500

    error page

--- error.vue
+++ error.vue
@@ -1,16 +1,14 @@
 <template>
-	<div>
-		<b-jumbotron
-			bg-variant="info"
-			text-variant="white"
-			border-variant="dark"
-			fluid
-			header="Error"
-		>
-			<p>error.statusMessage= {{ error.statusMessage }}</p>
-			<p>error.statusCode= {{ error.statusCode }}</p>
-			<b-button variant="primary" @click="handleError">Clear errors</b-button>
-		</b-jumbotron>
+	<div class="container py-4">
+		<div class="row">
+			<div class="h-100 p-5 text-white bg-dark rounded-2">
+				<p>error.statusMessage= {{ error.statusMessage }}</p>
+				<p>error.statusCode= {{ error.statusCode }}</p>
+				<button class="btn btn-primary btn-lg" @click="handleError">
+					Clear errors
+				</button>
+			</div>
+		</div>
 	</div>
 </template>
 
